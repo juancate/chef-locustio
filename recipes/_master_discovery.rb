328a9a8a169e739b4f87adfe6b698ab71b576355
Al Lefebvre <alain.lefebvre@lightspeedpos.com>
2015-06-09 08:59:04 -0400

    Fixed master node search query

--- recipes/_master_discovery.rb
+++ recipes/_master_discovery.rb
@@ -1,6 +1,8 @@
 
-locust_master = search('node', "recipe:locustio AND chef_environment:#{node.chef_environment} AND locustio_node_type:master")
+query = "recipe:locustio AND chef_environment:#{node.chef_environment} AND locustio_node_type:master"
+locust_master = search('node', query)
+puts "***** Locust Master Node Detected: ", locust_master
 
-fail "Backend declared with _discovery without search_query: #{backend_object.inspect}" if locust_master.length == 0
+fail "Backend declared with _discovery without search_query: #{query}" if locust_master.length == 0
 
 node.default['locustio']['master_ip'] = locust_master[0]['ipaddress']
